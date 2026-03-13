f872060541710bc577c7dda47b19ae79f3732f39
Ahamed Rumi <mailark@cock.li>
2026-03-13 22:18:19 +0530

    1->change how repo is managed 2->update hyprland animations

--- home/hyprland/config.nix
+++ home/hyprland/config.nix
@@ -57,19 +57,21 @@
 			enabled = true;
 
 			bezier = [
-				"easeOutQuint,0.23,1,0.32,1"
-					"easeInOutCubic,0.65,0.05,0.36,1"
-					"linear,0,0,1,1"
-					"almostLinear,0.5,0.5,0.75,1"
-					"quick,0.15,0,0.1,1"
+        "easeOutQuint,0.23,1,0.32,1"
+        "easeInOutCubic,0.65,0.05,0.36,1"
+        "linear,0,0,1,1"
+        "almostLinear,0.5,0.5,0.75,1"
+        "quick,0.15,0,0.1,1"
+        "smoothBounce, 0.34, 1.56, 0.64, 1.0"
+        "workspaceSlide, 0.25, 1.3, 0.5, 1.0"
 			];
 
 			animation = [
 				"global,1,10,default"
 					"border,1,5.39,easeOutQuint"
 					"windows,1,4.79,easeOutQuint"
-					"windowsIn,1,4.1,easeOutQuint,popin 87%"
-					"windowsOut,1,1.49,linear,popin 87%"
+					"windowsIn,1,4.1,smoothBounce,popin 87%"
+					"windowsOut,1,1.49,smoothBounce,popin 87%"
 					"fadeIn,1,1.73,almostLinear"
 					"fadeOut,1,1.46,almostLinear"
 					"fade,1,3.03,quick"
@@ -78,9 +80,9 @@
 					"layersOut,1,1.5,linear,fade"
 					"fadeLayersIn,1,1.79,almostLinear"
 					"fadeLayersOut,1,1.39,almostLinear"
-					"workspaces,1,1.94,almostLinear,fade"
-					"workspacesIn,1,1.21,almostLinear,fade"
-					"workspacesOut,1,1.94,almostLinear,fade"
+					"workspaces,1,4,workspaceSlide,slide"
+					"workspacesIn,1,4,workspaceSlide,slide"
+					"workspacesOut,1,4,workspaceSlide,slide"
 					"zoomFactor,1,7,quick"
 			];
 		};
---
+++ saveconf.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+DEST="/home/rumi/repo"
+USER_TO_OWN=${SUDO_USER:-root}
+
+bold=$(tput bold)
+red=$(tput setaf 1)
+reset=$(tput sgr0)
+
+echo "${bold}${red}-> Remove old copy of DEST if exists${reset}"
+set -x
+if [ -d "$DEST" ]; then
+  rm -rf "$DEST"
+fi
+
+set +x
+echo "${bold}${red}-> Create copy of /etc/nixos to DEST${reset}"
+set -x
+cp -a /etc/nixos "$DEST"
+chown -R "${USER_TO_OWN}:users" "$DEST"
+
+set +x
+echo "${bold}${red}-> Succesfully completed backup${reset}"
