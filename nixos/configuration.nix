f1b2c7647bdfa2e2fc295425f52bf78cadd20f56
Ahamed Rumi <mailark@cock.li>
2026-03-14 11:00:11 +0530

    add graphics drivers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -17,6 +17,12 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Graphics
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
