999a5d5defc552584e547d7438d57125ee76b673
Ahamed Rumi <mailark@cock.li>
2026-03-14 22:20:40 +0530

    [home/neovim] some options

--- nixos/home/neovim/config/init.lua
+++ nixos/home/neovim/config/init.lua
@@ -6,3 +6,8 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
 })
+
+vim.opt.nu = true;
+vim.opt.rnu = true;
+vim.opt.fcs = { eob = " " };
+vim.opt.cb = "unnamedplus";
